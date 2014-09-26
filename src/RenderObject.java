7e643d9c5b18c575d3f915d18282a5ad57ea8f08
jsandler18 <jsandler18@gmail.com>
2014-09-26 08:28:05 -0400

    RenderObject class
    added a class that will be a renderable object in the game

---
+++ src/RenderObject.java
@@ -0,0 +1,10 @@
+import java.awt.geom.Rectangle2D;
+
+
+public class RenderObject extends Rectangle2D.Double {
+
+	public RenderObject(double posx, double posy, double width, double height) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
